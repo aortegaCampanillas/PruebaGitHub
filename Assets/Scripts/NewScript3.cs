d801021923aee2b0411b27e268854d0897464a1d
aortegaCampanillas <antonio.ortega@iescampanillas.com>
2021-12-30 12:29:18 +0100

    Se añade Script3

--- Assets/Scripts/NewScript3.cs
+++ Assets/Scripts/NewScript3.cs
@@ -7,7 +7,7 @@ public class NewScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+	// Comentario en start        
     }
 
     // Update is called once per frame

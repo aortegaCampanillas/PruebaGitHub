30d73ce181535cdfa346c4f9a86f9c9388054388
aortegaCampanillas <antonio.ortega@iescampanillas.com>
2021-12-30 12:32:27 +0100

    Nuevo comentario

--- Assets/Scripts/OtherScript.cs
+++ Assets/Scripts/OtherScript.cs
@@ -7,7 +7,7 @@ public class NewScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Comentario
     }
 
     // Update is called once per frame

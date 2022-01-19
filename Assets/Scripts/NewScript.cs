dc57e4cdd8134368d823053b21c19c21adb80a91
usuario <usuario@debian-usuario.localdomain>
2022-01-19 23:56:24 +0100

    comentario aortega

--- Assets/Scripts/NewScript.cs
+++ Assets/Scripts/NewScript.cs
@@ -7,7 +7,7 @@ public class NewScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // comentario aortega
     }
 
     // Update is called once per frame
